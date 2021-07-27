27c6108b0640e1034e90058bd7f371c96ce2255e
Ricardo Moreira <ricardo.1315.m@gmail.com>
2021-07-27 23:46:32 +0100

    Add requirements

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
+import pathlib
 import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
+
+HERE = pathlib.Path(__file__).parent
 
 setuptools.setup(
     name="uncertain-trees",
@@ -20,6 +23,7 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=open(HERE / "src" / "requirements.txt").readlines(),
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
     python_requires=">=3.8",
